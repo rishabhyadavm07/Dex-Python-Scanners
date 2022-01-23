daa3a54aa55659b11a5907beb9121914c136c9e3
Rishabh <rishabhyadavm07@gmail.com>
2022-01-23 17:11:45 +0530

    Added raw inputs

--- SimplePortScanner.py
+++ SimplePortScanner.py
@@ -5,10 +5,14 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #the above program line makes a socked object for IPv4 - socket.AF_INET, socket.SOCK_STREAM - if for using TCP protocol
 
+# now we will be adding raw inputs functionality to this program
+IP = input("Enter the IP you want to scan: ")
+pt= input("Enter the port you want to run the scan on: ")
+
 # now we will be declaring some variables to feed to the program
 
-Host = "127.0.0.1"
-Port = 80
+Host = IP
+Port = int(pt)
 
 # now we will be making a function that will try to connect to a port number present in the port variable
 
